766e0e5a4df01705478fc11c3bccfe84948779d8
Juliana Marques <marquesdjuliana@gmail.com>
2023-10-03 19:03:46 -0300

    Increases Matches test coverage

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -7,6 +7,7 @@ import { app } from '../app';
 
 import { matches } from './matches.mock';
 import SequelizeMatch from '../database/models/SequelizeMatch';
+import TokenService from '../utils/authenticateToken';
 
 
 chai.use(chaiHttp);
@@ -23,6 +24,42 @@ describe('Matches test', () => {
 
     expect(status).to.equal(200);
     expect(body).to.deep.equal(matches)
+  });
+  it('Should return matches by progress', async () => {
+    const inProgress = true; 
+    sinon.stub(SequelizeMatch, 'findAll').resolves(matches as any);
+
+    const { status, body } = await chai.request(app).get(`/matches?inProgress=${inProgress}`);
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matches);
+  });
+
+  it('Should finish a match with valid token', async () => {
+    const matchId = 1;
+    const token = TokenService.generateToken({ id: 1 }); 
+
+    sinon.stub(SequelizeMatch, 'update').resolves([1]);
+    sinon.stub(TokenService, 'decodeToken').returns({ id: 1 });
+
+    const { status, body } = await chai.request(app)
+      .patch(`/matches/${matchId}/finish`) 
+      .set('Authorization', `Bearer ${token}`) 
+      .send({ homeTeamGoals: 3, awayTeamGoals: 1 }); 
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal({ message: 'Finished' });
+  });
+  it('Should return 401 with invalid token', async () => {
+    const matchId = 1;
+    const invalidToken = 'token_invalido';
+
+    const { status, body } = await chai.request(app)
+      .patch(`/matches/${matchId}/finish`)
+      .set('Authorization', `Bearer ${invalidToken}`);
+
+    expect(status).to.equal(401);
+    expect(body).to.deep.equal({ message: 'Token must be a valid token' });
   });
 
 });
